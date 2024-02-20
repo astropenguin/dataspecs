8e7444317485d4ba5b7edccf17b958b98a3fe506
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-02-20 17:19:35 +0000

    #9 Rename argument (tp → hint)

--- dataspecs/typing.py
+++ dataspecs/typing.py
@@ -21,49 +21,49 @@ class TagBase(Enum):
     """Base enum for specification tags."""
 
 
-def get_annotated(tp: Any) -> Any:
-    """Return annotated type of a type if it exists."""
-    return get_args(tp)[0] if is_annotated(tp) else tp
+def get_annotated(hint: Any) -> Any:
+    """Return annotated type of a type hint if it exists."""
+    return get_args(hint)[0] if is_annotated(hint) else hint
 
 
-def get_annotations(tp: Any) -> tuple[Any, ...]:
-    """Return annotations of a type if they exist."""
-    return get_args(tp)[1:] if is_annotated(tp) else ()
+def get_annotations(hint: Any) -> tuple[Any, ...]:
+    """Return annotations of a type hint if they exist."""
+    return get_args(hint)[1:] if is_annotated(hint) else ()
 
 
-def get_dataclasses(tp: Any) -> tuple[DataClass, ...]:
-    """Return dataclass objects that annotate a type."""
-    return tuple(filter(is_dataclass, get_annotations(tp)))
+def get_dataclasses(hint: Any) -> tuple[DataClass, ...]:
+    """Return dataclass objects that annotate a type hint."""
+    return tuple(filter(is_dataclass, get_annotations(hint)))
 
 
-def get_first(tp: Any) -> Any:
-    """Return the first type if a type is a union of types."""
-    return get_args(tp)[0] if is_union(tp) else tp
+def get_first(hint: Any) -> Any:
+    """Return the first type if a type hint is a union type."""
+    return get_args(hint)[0] if is_union(hint) else hint
 
 
-def get_literals(tp: Any) -> Any:
-    """Return literals if a type is a literal type."""
-    return get_args(tp) if is_literal(tp) else tp
+def get_literals(hint: Any) -> Any:
+    """Return literals if a type hint is a literal type."""
+    return get_args(hint) if is_literal(hint) else hint
 
 
-def get_subscriptions(tp: Any) -> tuple[Any, ...]:
-    """Return subscriptions of a type if they exist."""
-    return get_args(get_annotated(tp))
+def get_subscriptions(hint: Any) -> tuple[Any, ...]:
+    """Return subscriptions of a type hint if they exist."""
+    return get_args(get_annotated(hint))
 
 
-def get_tags(tp: Any) -> tuple[TagBase, ...]:
-    """Return tags that annotate a type."""
-    return tuple(filter(is_tag, get_annotations(tp)))
+def get_tags(hint: Any) -> tuple[TagBase, ...]:
+    """Return tags that annotate a type hint."""
+    return tuple(filter(is_tag, get_annotations(hint)))
 
 
-def is_annotated(tp: Any) -> bool:
-    """Check if a type is annotated."""
-    return get_origin(tp) is Annotated
+def is_annotated(hint: Any) -> bool:
+    """Check if a type hint is annotated."""
+    return get_origin(hint) is Annotated
 
 
-def is_literal(tp: Any) -> bool:
-    """Check if a type is a literal type."""
-    return get_origin(tp) is Literal
+def is_literal(hint: Any) -> bool:
+    """Check if a type hint is a literal type."""
+    return get_origin(hint) is Literal
 
 
 def is_tag(obj: Any) -> TypeGuard[TagBase]:
@@ -71,6 +71,6 @@ def is_tag(obj: Any) -> TypeGuard[TagBase]:
     return isinstance(obj, TagBase)
 
 
-def is_union(tp: Any) -> bool:
-    """Check if a type is a union type."""
-    return get_origin(Union[tp]) is Union  # type: ignore
+def is_union(hint: Any) -> bool:
+    """Check if a type hint is a union type."""
+    return get_origin(Union[hint]) is Union  # type: ignore
