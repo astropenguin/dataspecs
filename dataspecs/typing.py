ed4cc2ef5ca23410862cb06f3dd6f29a53eef19e
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-03-13 08:55:07 +0000

    #20 Add option for ignoring annotations to get_first

--- dataspecs/typing.py
+++ dataspecs/typing.py
@@ -56,9 +56,12 @@ def get_dataclasses(obj: Any, /) -> tuple[DataClass, ...]:
     return tuple(filter(is_dataclass, get_annotations(obj)))
 
 
-def get_first(obj: Any, /) -> Any:
+def get_first(obj: Any, /, ignore_annotations: bool = False) -> Any:
     """Return the first type if a type hint is a union type."""
-    return get_args(obj)[0] if is_union(obj) else obj
+    if ignore_annotations and is_union(annotated := get_annotated(obj)):
+        return get_first(get_args(annotated)[0], ignore_annotations)
+    else:
+        return get_args(obj)[0] if is_union(obj) else obj
 
 
 def get_subscriptions(obj: Any, /) -> tuple[Any, ...]:
