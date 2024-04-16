923ddce764c159712d0cb03ac5bcbedba904d006
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-04-16 15:15:46 +0000

    #14 Add tests for extras/replacement module

---
+++ tests/test_extras_replacement.py
@@ -0,0 +1,38 @@
+# standard library
+from dataclasses import dataclass
+from enum import auto
+from typing import Annotated, Optional
+
+
+# dependencies
+from dataspecs import TagBase, from_dataclass
+from dataspecs.extras import Replace, replace
+
+
+# test datasets
+class Tag(TagBase):
+    A = auto()
+
+
+@dataclass
+class Test:
+    a: Annotated[float, Tag.A]
+    a_type: Annotated[Optional[type], Replace("/a", "type")] = None
+    a_data: Annotated[Optional[float], Replace("/a", "data")] = None
+
+
+# test functions
+def test_replace() -> None:
+    specs = from_dataclass(Test(0, int, 1))
+    replaced = replace(specs)
+
+    assert replaced[0].type is int
+    assert replaced[0].data == 1
+
+
+def test_replace_skip() -> None:
+    specs = from_dataclass(Test(0, None, None))
+    replaced = replace(specs)
+
+    assert replaced[0].type is float
+    assert replaced[0].data == 0
