dcab16422e9addcd01e0543a1212b0b029c7dda6
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-04-16 15:13:58 +0000

    #14 Add data class and function for replacement

--- dataspecs/extras/__init__.py
+++ dataspecs/extras/__init__.py
@@ -1,5 +1,15 @@
-__all__ = ["replacement"]
+__all__ = [
+    # submodules
+    "replacement",
+    # aliases
+    "Replace",
+    "replace",
+]
 
 
 # submodules
 from . import replacement
+
+
+# aliases
+from .replacement import *
