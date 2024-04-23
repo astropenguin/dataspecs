92954b2cc5c2937daa10b89291f33ab56d2f7743
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2024-04-23 15:09:27 +0000

    #29 Reorder keyword-only arguments of api functions

--- dataspecs/core/api.py
+++ dataspecs/core/api.py
@@ -29,10 +29,10 @@ def from_dataclass(
     obj: DataClass,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
 ) -> Specs[Spec]: ...
 
 
@@ -41,10 +41,10 @@ def from_dataclass(
     obj: DataClass,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
     spec_factory: Callable[..., TSpec],
 ) -> Specs[TSpec]: ...
 
@@ -53,23 +53,23 @@ def from_dataclass(
     obj: DataClass,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
     spec_factory: Any = Spec,
 ) -> Any:
     """Create data specs from a dataclass object.
 
     Args:
         obj: Dataclass object to be parsed.
-        parent_id: ID of the parent data spec.
         first_only: If ``True`` and a type hint is a union of types,
             parse the first type only instead of the whole type hint.
         tagged_only: If ``True``, drop leaf (i.e. terminal) and
             adjacent superior data specs that do not have any tags.
         type_only: If ``True``, each data spec type contains
             a type hint with all annotation removed.
+        parent_id: ID of the parent data spec.
         spec_factory: Factory for creating each data spec.
 
     Returns:
@@ -92,10 +92,10 @@ def from_dataclass(
         specs.extend(
             from_typehint(
                 reftype,
-                parent_id=child_id,
                 first_only=first_only,
                 tagged_only=tagged_only,
                 type_only=type_only,
+                parent_id=child_id,
                 spec_factory=spec_factory,
             )
         )
@@ -108,10 +108,10 @@ def from_typehint(
     obj: Any,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
 ) -> Specs[Spec]: ...
 
 
@@ -120,10 +120,10 @@ def from_typehint(
     obj: Any,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
     spec_factory: Callable[..., TSpec],
 ) -> Specs[TSpec]: ...
 
@@ -132,23 +132,23 @@ def from_typehint(
     obj: Any,
     /,
     *,
-    parent_id: StrPath = ROOT,
     first_only: bool = True,
     tagged_only: bool = True,
     type_only: bool = True,
+    parent_id: StrPath = ROOT,
     spec_factory: Any = Spec,
 ) -> Any:
     """Create data specs from a type hint.
 
     Args:
         obj: Type hint to be parsed.
-        parent_id: ID of the parent data spec.
         first_only: If ``True`` and a type hint is a union of types,
             parse the first type only instead of the whole type hint.
         tagged_only: If ``True``, drop leaf (i.e. terminal) and
             adjacent superior data specs that do not have any tags.
         type_only: If ``True``, each data spec type contains
             a type hint with all annotation removed.
+        parent_id: ID of the parent data spec.
         spec_factory: Factory for creating each data spec.
 
     Returns:
@@ -168,10 +168,10 @@ def from_typehint(
         specs.extend(
             from_typehint(
                 subtype,
-                parent_id=child_id,
                 first_only=first_only,
                 tagged_only=tagged_only,
                 type_only=type_only,
+                parent_id=child_id,
                 spec_factory=spec_factory,
             )
         )
@@ -180,10 +180,10 @@ def from_typehint(
         specs.extend(
             from_dataclass(
                 dataclass,
-                parent_id=parent_id,
                 first_only=first_only,
                 tagged_only=tagged_only,
                 type_only=type_only,
+                parent_id=parent_id,
                 spec_factory=spec_factory,
             )
         )
