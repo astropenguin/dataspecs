0bf66ad0724b852807e878ef439c1592a8465007
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-02-23 11:36:36 +0000

    #97 Remove extras subpackage

--- dataspecs/__init__.py
+++ dataspecs/__init__.py
@@ -1,42 +1,27 @@
 __all__ = [
     # subpackages
     "core",
-    "extras",
     # aliases (core)
-    "ROOT",
-    "Path",
+    "Attr",
+    "ID",
+    "Name",
     "Spec",
     "Specs",
-    "StrPath",
-    "TagBase",
+    "Tag",
+    "Type",
+    "Unit",
+    "Value",
     "from_dataclass",
     "from_typehint",
-    # aliases (extras)
-    "Format",
-    "FormatTag",
-    "Name",
-    "NameTag",
-    "Replace",
-    "ReplaceTag",
-    "format",
-    "name",
-    "replace",
 ]
 __version__ = "5.0.0"
 
 
 # subpackages
 from . import core
-from . import extras
 
 
 # aliases (core)
 from .core.api import *
+from .core.spec import *
 from .core.specs import *
-from .core.typing import *
-
-
-# aliases (extras)
-from .extras.formatting import *
-from .extras.naming import *
-from .extras.replacing import *
