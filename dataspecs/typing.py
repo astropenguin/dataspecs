7d72781d16fc462d6809f6ef2a3aec7f7381e5b8
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2025-06-06 15:16:52 +0000

    #99 Update typing submodule

--- dataspecs/typing.py
+++ dataspecs/typing.py
@@ -1,131 +1,79 @@
-__all__ = ["StrPath", "TagBase"]
+__all__ = []
 
 
 # standard library
+import builtins
 import types
-from dataclasses import Field, is_dataclass
-from enum import Enum
+from collections.abc import Callable, Iterator
+from dataclasses import Field
 from os import PathLike
-from typing import Annotated, Any, ClassVar, Literal, Protocol, TypeVar, Union
+from typing import Annotated, Any, ClassVar, Literal, Protocol, Union
+from typing import _strip_annotations  # type: ignore
 
 
 # dependencies
-from typing_extensions import TypeGuard, get_args, get_origin
+from typing_extensions import get_args, get_origin
 
 
 # type hints
-StrPath = Union[str, PathLike[str]]
-TAny = TypeVar("TAny")
-UAny = TypeVar("UAny")
-
-
-class DataClassObject(Protocol):
-    """Type hint for any dataclass object."""
+class DataClassInstance(Protocol):
+    """Type hint for any data-class instance."""
 
     __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
 
 
-DataClass = Union[DataClassObject, type[DataClassObject]]
-"""Type hint for any dataclass or dataclass object."""
+DataClass = Union[DataClassInstance, type[DataClassInstance]]
+"""Type hint for any data class or data-class instance."""
 
 
-class TagBase(Enum):
-    """Base enum of tag for data specs.
-
-    Since ``TagBase`` itself does not have any members,
-    users should create their own tags by inheriting it::
-
-        from enum import auto
-        from dataspecs import TagBase
-
-        class Tag(TagBase):
-            ATTR = auto()
-            DATA = auto()
-            NAME = auto()
-
-    """
-
-    pass
+StrPath = Union[PathLike[str], str]
+"""Type hint for any data that can be a file system path."""
 
 
-def get_annotated(obj: Any, /, *, recursive: bool = False) -> Any:
-    """Return annotated type of a type hint if it exists."""
-    if recursive:
-        from typing import _strip_annotations  # type: ignore
-
-        return _strip_annotations(obj)  # type: ignore
-    else:
-        return get_args(obj)[0] if is_annotated(obj) else obj
-
-
-def get_annotations(obj: Any, /) -> tuple[Any, ...]:
-    """Return annotations of a type hint if they exist."""
-    return get_args(obj)[1:] if is_annotated(obj) else ()
-
-
-def get_dataclasses(obj: Any, /) -> tuple[DataClass, ...]:
-    """Return annotations of dataclasses (objects)."""
-    return tuple(filter(is_dataclass, get_annotations(obj)))
+def gen_annotations(
+    obj: Any,
+    /,
+    *,
+    filter: Callable[[Any], bool] = lambda obj: True,
+) -> Iterator[Any]:
+    """Generate annotations if given object is an annotated type."""
+    if is_annotated(obj):
+        annotated, *annotations = get_args(obj)
+        yield from gen_annotations(annotated, filter=filter)
+        yield from builtins.filter(filter, annotations)
+    elif is_union(obj):
+        for arg in get_args(obj):
+            yield from gen_annotations(arg, filter=filter)
 
 
-def get_first(obj: Any, /) -> Any:
-    """Return the first type if a type hint is a union type."""
-    if is_union(annotated := get_annotated(obj)):
-        first = get_args(annotated)[0]
-    else:
-        first = annotated
-
-    for annotation in get_annotations(obj):
-        first = Annotated[first, annotation]
-
-    return first
+def gen_subtypes(obj: Any, /) -> Iterator[Any]:
+    """Generate subtypes if given object is a generic type."""
+    if is_annotated(obj):
+        yield from gen_subtypes(get_args(obj)[0])
+    elif is_union(obj):
+        for arg in get_args(obj):
+            yield from gen_subtypes(arg)
+    elif not is_literal(obj):
+        yield from get_args(obj)
 
 
-def get_subtypes(obj: Any, /) -> tuple[Any, ...]:
-    """Return subtypes of a type hint if they exist."""
-    if is_literal(annotated := get_annotated(obj)):
-        return ()
-
-    return get_args(annotated)
-
-
-def get_tags(obj: Any, /) -> tuple[TagBase, ...]:
-    """Return annotations of tags for data specs."""
-    return tuple(filter(is_tag, get_annotations(obj)))
+def get_annotated(obj: Any, /) -> Any:
+    """Return the bare type if given object is an annotated type."""
+    return _strip_annotations(obj)  # type: ignore
 
 
 def is_annotated(obj: Any, /) -> bool:
-    """Check if a type hint is annotated."""
+    """Check if given object is an annotated type."""
     return get_origin(obj) is Annotated
 
 
-def is_anytype(obj: Any, /) -> TypeGuard[type[Any]]:
-    """Check if an object is an any type."""
-    return isinstance(obj, type)
-
-
 def is_literal(obj: Any, /) -> bool:
-    """Check if a type hint is a literal type."""
+    """Check if given object is a literal type."""
     return get_origin(obj) is Literal
 
 
-def is_strpath(obj: Any, /) -> TypeGuard[StrPath]:
-    """Check if an object is a string or a string path."""
-    return isinstance(obj, (str, PathLike))
-
-
-def is_tag(obj: Any, /) -> TypeGuard[TagBase]:
-    """Check if an object is a specification tag."""
-    return isinstance(obj, TagBase)
-
-
-def is_tagtype(obj: Any, /) -> TypeGuard[type[TagBase]]:
-    """Check if an object is a specification tag type."""
-    return isinstance(obj, type) and issubclass(obj, TagBase)
-
-
 def is_union(obj: Any, /) -> bool:
-    """Check if a type hint is a union type."""
+    """Check if given object is a union type."""
     if (UnionType := getattr(types, "UnionType", None)) is not None:
         # For Python >= 3.10
         return get_origin(obj) is Union or isinstance(obj, UnionType)
--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,198 +1,54 @@
 # standard library
-from dataclasses import dataclass
-from enum import Enum, auto
-from pathlib import Path
-from typing import Annotated as Ann, Any, Literal, Optional, Union
+from sys import version_info as ver
+from typing import Annotated as Ann, Literal as L, Union
 
 
 # dependencies
 from dataspecs.typing import (
-    TagBase,
+    gen_annotations,
+    gen_subtypes,
     get_annotated,
-    get_annotations,
-    get_dataclasses,
-    get_first,
-    get_subtypes,
-    get_tags,
     is_annotated,
-    is_anytype,
     is_literal,
-    is_strpath,
-    is_tag,
-    is_tagtype,
     is_union,
 )
-from pytest import mark
-
-
-# type hints
-TestData = list[tuple[Any, ...]]
-
-
-# test datasets
-class NonTag(Enum):
-    A = auto()
-    B = auto()
-
-
-class Tag(TagBase):
-    A = auto()
-    B = auto()
-
-
-@dataclass
-class DC:
-    a: int
-
-
-data_get_annotated: TestData = [
-    (Ann[int, "ann"], int),
-    (int, int),
-]
-
-data_get_annotations: TestData = [
-    (Ann[int, DC(1), DC(2)], (DC(1), DC(2))),
-    (Ann[int, DC(1), Tag.A], (DC(1), Tag.A)),
-    (Ann[int, DC(1)], (DC(1),)),
-    (int, ()),
-]
-
-data_get_dataclasses: TestData = [
-    (Ann[int, DC(1), DC(2)], (DC(1), DC(2))),
-    (Ann[int, DC(1), Tag.A], (DC(1),)),
-    (Ann[int, DC(1)], (DC(1),)),
-    (int, ()),
-]
-
-data_get_first: TestData = [
-    (Union[int, None], int),
-    (Optional[int], int),
-    (int, int),
-    (Ann[Union[int, None], Tag.A], Ann[int, Tag.A]),
-    (Ann[Optional[int], Tag.A], Ann[int, Tag.A]),
-    (Ann[int, Tag.A], Ann[int, Tag.A]),
-]
-
-data_get_subtypes: TestData = [
-    (Union[str, int], (str, int)),
-    (dict[str, int], (str, int)),
-    (list[int], (int,)),
-    (Literal[1], ()),
-    (int, ()),
-]
-
-data_get_tags: TestData = [
-    (Ann[int, Tag.A, Tag.B], (Tag.A, Tag.B)),
-    (Ann[int, Tag.A, NonTag.B], (Tag.A,)),
-    (Ann[int, Tag.A], (Tag.A,)),
-    (Ann[int, NonTag.A], ()),
-]
-
-data_is_annotated: TestData = [
-    (Ann[int, "ann"], True),
-    (int, False),
-]
-
-data_is_anytype: TestData = [
-    (int, True),
-    (Ann[int, "ann"], False),
-    (1, False),
-]
-
-data_is_literal: TestData = [
-    (Literal[1], True),
-    (1, False),
-]
-
-data_is_strpath: TestData = [
-    ("path", True),
-    (Path("path"), True),
-    (b"path", False),
-    (1, False),
-]
-
-data_is_tag: TestData = [
-    (Tag.A, True),
-    (NonTag.A, False),
-    (1, False),
-]
-
-data_is_tagtype: TestData = [
-    (Tag, True),
-    (Tag.A, False),
-    (NonTag, False),
-    (NonTag.A, False),
-    (1, False),
-]
-
-data_is_union: TestData = [
-    (Ann[Union[int, None], "ann"], False),
-    (Union[int, None], True),
-    (int, False),
-]
 
 
 # test functions
-@mark.parametrize("tester, expected", data_get_annotated)
-def test_get_annotated(tester: Any, expected: Any) -> None:
-    assert get_annotated(tester) == expected
+def test_gen_annotations() -> None:
+    assert list(gen_annotations(int)) == []
+    assert list(gen_annotations(Ann[int, 0])) == [0]
+    assert list(gen_annotations(Union[Ann[int, 0], Ann[int, 1]])) == [0, 1]
+    assert list(gen_annotations(Ann[Union[Ann[int, 0], Ann[int, 1]], 2])) == [0, 1, 2]
 
 
-@mark.parametrize("tester, expected", data_get_annotations)
-def test_get_annotations(tester: Any, expected: Any) -> None:
-    assert get_annotations(tester) == expected
+def test_gen_subtypes() -> None:
+    assert list(gen_subtypes(int)) == []
+    assert list(gen_subtypes(dict[str, int])) == [str, int]
+    assert list(gen_subtypes(dict[str, L[0, 1]])) == [str, L[0, 1]]
+    assert list(gen_subtypes(Union[dict[str, int], None])) == [str, int]
 
 
-@mark.parametrize("tester, expected", data_get_dataclasses)
-def test_get_dataclasses(tester: Any, expected: Any) -> None:
-    assert get_dataclasses(tester) == expected
+def test_get_annotated() -> None:
+    assert get_annotated(int) == int
+    assert get_annotated(Ann[int, 0]) == int
+    assert get_annotated(Union[Ann[int, 0], Ann[int, 1]]) == int
+    assert get_annotated(Ann[Union[Ann[int, 0], Ann[int, 1]], 2]) == int
 
 
-@mark.parametrize("tester, expected", data_get_first)
-def test_get_first(tester: Any, expected: Any) -> None:
-    assert get_first(tester) == expected
+def test_is_annotated() -> None:
+    assert not is_annotated(int)
+    assert is_annotated(Ann[int, 0])
 
 
-@mark.parametrize("tester, expected", data_get_subtypes)
-def test_get_subtypes(tester: Any, expected: Any) -> None:
-    assert get_subtypes(tester) == expected
+def test_is_literal() -> None:
+    assert not is_literal(int)
+    assert is_literal(L[0])
 
 
-@mark.parametrize("tester, expected", data_get_tags)
-def test_get_tags(tester: Any, expected: Any) -> None:
-    assert get_tags(tester) == expected
+def test_is_union() -> None:
+    assert not is_union(int)
+    assert is_union(Union[int, float])
 
-
-@mark.parametrize("tester, expected", data_is_annotated)
-def test_is_annotated(tester: Any, expected: bool) -> None:
-    assert is_annotated(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_anytype)
-def test_is_anytype(tester: Any, expected: bool) -> None:
-    assert is_anytype(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_literal)
-def test_is_literal(tester: Any, expected: bool) -> None:
-    assert is_literal(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_strpath)
-def test_is_strpath(tester: Any, expected: bool) -> None:
-    assert is_strpath(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_tag)
-def test_is_tag(tester: Any, expected: bool) -> None:
-    assert is_tag(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_tagtype)
-def test_is_tagtype(tester: Any, expected: bool) -> None:
-    assert is_tagtype(tester) == expected
-
-
-@mark.parametrize("tester, expected", data_is_union)
-def test_is_union(tester: Any, expected: Any) -> None:
-    assert is_union(tester) == expected
+    if ver.major >= 3 and ver.minor >= 10:
+        assert is_union(int | float)
